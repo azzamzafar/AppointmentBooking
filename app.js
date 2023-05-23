fc641e1ca378a359c29a75a3f7140c90a06e444b
Azzam Zafar <azzam.zafar21@gmail.com>
2023-05-23 15:10:58 +0530

    custom middleware for solving CORS

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
-const cors = require('cors');
+// const cors = require('cors');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const { sequelize } = require('./utils/db_con.js')
 
 const userRoute = require('./routes/user.js')
-const allowedOrigins = ['http://127.0.0.1:5500'];
-app.use(cors({
-  origin: allowedOrigins
-}));
 
+// const allowedOrigins = ['localhost:8000','http://127.0.0.1:8000','http://127.0.0.1:5500'];
+// app.use(cors({
+//   origin: allowedOrigins
+// }));
+const setCors = (req,res,next)=>{
+  res.setHeader('Content-Type','application/json')
+  res.setHeader('Access-Control-Allow-Origin','http://127.0.0.1:8000')
+  res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,DELETE')
+  res.setHeader('Access-Control-Allow-Headers','Content-Type')
+  res.setHeader('Vary','origin')
+  next()
+}
+app.use(setCors)
 app.use(bodyParser.json({extended: false}));
 
-app.use('/users',userRoute);
+app.use('/users',setCors,userRoute);
 
 sequelize.sync()
 .then(()=>app.listen(3000))
--- client/network_calls.mjs
+++ client/network_calls.mjs
@@ -1,5 +1,5 @@
 export const axiosInstance = axios.create({
-    baseURL:'http://127.0.0.1:3000/',
+    baseURL:'http://127.0.0.1:3000/'
     
 })
 
